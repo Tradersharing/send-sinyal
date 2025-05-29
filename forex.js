8a0fe894239fd5f4bb43a8bf94beaacd8e3a7f4e
Tradersharing <ardiyulistyanto3@gmail.com>
2025-05-30 01:48:46 +0700

    Update forex.js

--- forex.js
+++ forex.js
@@ -1,8 +1,6 @@
 const { createClient } = require('@supabase/supabase-js');
 
 // Data koneksi Supabase kamu
-const SUPABASE_URL = 'https://oaatowhxrefpjlwucvvg.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9hYXRvd2h4cmVmcGpsd3VjdnZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0MzgzMDQsImV4cCI6MjA2NDAxNDMwNH0.-Qf6y5JiWVx2PAVkMej9Y6qXHr5WSTlJwqG9GInPD7g';
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
