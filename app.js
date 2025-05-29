3539e159841c4b2a9fb083b1658d35d4314c8e8e
Tradersharing <ardiyulistyanto3@gmail.com>
2025-05-29 13:02:06 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-// Ganti nama tabel jika perlu
-const TABLE_NAME = 'users';
+// Nama tabel di Supabase project "bot signal"
+const TABLE_NAME = 'usersignal'; // Pastikan nama tabel sesuai di dashboard
 
 const SUPABASE_URL = 'https://oaatowhxrefpjlwucvvg.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9hYXRvd2h4cmVmcGpsd3VjdnZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0MzgzMDQsImV4cCI6MjA2NDAxNDMwNH0.-Qf6y5JiWVx2PAVkMej9Y6qXHr5WSTlJwqG9GInPD7g';
